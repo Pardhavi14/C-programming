9741f42a571e30b0bbfb1a99b8ce6a3111acd3f3
Pardhavi14 <114010821+Pardhavi14@users.noreply.github.com>
2024-10-13 23:16:49 +0530

    Add files via upload

---
+++ realloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float n,sum=0;
+    int i;
+    printf("enter the number of float values:");
+    scanf("%f",&n);
+    float *ptr=(float*)calloc(n,sizeof(float));
+    
+    printf("the value present at first byte is:%f",*(ptr+5));
+    
+    if(ptr==NULL){
+        printf("memory is not allocated");
+        exit(0);
+    }else{
+        float c,newsize;
+        printf("enter the extended number of values you want to enter:");
+        scanf("%f",&c);
+        
+        newsize=n+c;
+        ptr=(float*)realloc(ptr,newsize*sizeof(float));
+        
+        
+        for(i=0;i<n;i++){
+            printf("enter the float number%d:",i+1);
+            scanf("%f",ptr+i);
+        }
+        
+        for(i=n;i<newsize;i++){
+            printf("enter the extended float values%d:",i+1);
+            scanf("%f",ptr+i);
+        }
+        for(i=0;i<newsize;i++){
+            sum += *(ptr+i);
+        }
+        printf("the sum of given float values are:%.2f",sum);
+    }
+    return 0;
+}
